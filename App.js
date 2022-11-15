b396ff56f516109dc01207c613a85ec5fa42d245
armia-roma <armia_bos@hotmail.com>
2022-11-16 00:01:12 +0200

    new some edit

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from "react";
 // import WelcomeScreen from "./app/screen/WelcomeScreen";
-import ViewImageScreen from "./app/screen/ViewImageScreen";
-
+// import ViewImageScreen from "./app/screen/ViewImageScreen";
+import TestPage from "./app/screen/TestPage";
+import WelcomeScreen from "./app/screen/WelcomeScreen";
 
 export default function App() {
-    return <ViewImageScreen />
+    // return <TestPage />
+    return <WelcomeScreen></WelcomeScreen>
     
 }
--- app/screen/WelcomeScreen.js
+++ app/screen/WelcomeScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { ImageBackground,Text, Image, StyleSheet, View } from 'react-native';
+import AppButton from '../components/AppButton';
 
 function WelcomeScreen(props) {
     return (
         <ImageBackground 
+        blurRadius={10}
             source={require('../assets/back.jpg')}
             style={styles.background}
         >
@@ -13,8 +15,11 @@ function WelcomeScreen(props) {
                     style={styles.logo}></Image>
                 <Text style={styles.text}>Sell What You Don't Need</Text>
             </View>
-            <View style={styles.loginButton }/>
-            <View style={styles.registerButton }/>
+            <View style={styles.buttonContainer}>
+                <AppButton title="Login"/>
+                <AppButton title="Register" color='secondary'/>
+
+            </View>
         </ImageBackground>
     );
 }
@@ -24,20 +29,21 @@ const styles = StyleSheet.create({
             justifyContent: "flex-end",
             alignItems: "center" 
         },
-        loginButton: {
-            width: "100%",
-            height: 70,
-            backgroundColor: "#fc5c65",
-        },
-        registerButton: {
-            width: "100%",
-            height: 70,
-            backgroundColor: "#4ecdc4"
-        },
+        // loginButton: {
+        //     width: "100%",
+        //     height: 70,
+        //     backgroundColor: "#fc5c65",
+        // },
+        // registerButton: {
+        //     width: "100%",
+        //     height: 70,
+        //     backgroundColor: "#4ecdc4"
+        // },
         logo: {
             width: 200,
             height: 200,
-            backgroundColor: "#4ecdc4"
+            backgroundColor: "#4ecdc4",
+            borderRadius: 30,
 
         },logoContainer: {
                 position: 'absolute',
@@ -47,7 +53,13 @@ const styles = StyleSheet.create({
         text: {
             backgroundColor: "#4ecdc4",
             position: 'absolute',
-            bottom: 8
+            bottom: 8,
+            fontSize: 15,
+            fontWeight: "600"
+        },
+        buttonContainer: {
+            padding: 20,
+            width: "100%"
         }
 })
 
